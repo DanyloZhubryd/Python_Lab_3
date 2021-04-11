83a65e63cf2cf9127def3eda0c78010de53b3def
Zhubryd Danylo <zubdan21@gmail.com>
2021-04-11 19:55:50 +0300

    Changing from SQLite to MySQL

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os
 
 # Init app
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'mysql+mysqlconnector://myuser:aboba@localhost:3306/Lab6'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
